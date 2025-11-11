4e9dbd870b7fcd1319566b5ecd5f18d984ee69a1
Kingsley Victor <javaprodigy56@gmail.com>
2025-11-11 07:31:31 +0100

    chore: create routa ev contract

--- src/ROUTA.sol
+++ src/ROUTA.sol
@@ -4,14 +4,24 @@ import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Permit.sol";
 import "./interfaces/IROUTA.sol";
 
 contract ROUTA is ERC20Permit, IROUTA {
+    address public immutable minter;
+
     constructor(
         address to,
-        uint256 amount
+        uint256 amount,
+        address _minter
     ) ERC20Permit("Routa EV") ERC20("Routa EV", "ROUTA") {
         _mint(to, amount);
+        minter = _minter;
     }
 
     function mint(address to, uint256 amount) external {
+        if (msg.sender != minter) revert OnlyMinter();
         _mint(to, amount);
+    }
+
+    function burn(address to, uint256 amount) external {
+        if (msg.sender != minter) revert OnlyMinter();
+        _burn(to, amount);
     }
 }
--- src/interfaces/IROUTA.sol
+++ src/interfaces/IROUTA.sol
@@ -1,5 +1,9 @@
 pragma solidity >=0.8.28;
 
 interface IROUTA {
+    error OnlyMinter();
+
+    function minter() external view returns (address);
+
     function mint(address to, uint256 amount) external;
 }
